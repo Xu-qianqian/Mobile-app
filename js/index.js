f8806d42378a9e7c05e7db84ca5e319f1cd57f75
Xu-qianqian <you@example.com>
2018-09-30 13:56:55 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+    var intDiff = parseInt(106808);//倒计时总秒数量
+function timer(intDiff){
+    window.setInterval(function(){
+    var day=0,
+        hour=0,
+        minute=0,
+        second=0;//时间默认值        
+    if(intDiff > 0){
+        day = Math.floor(intDiff / (60 * 60 * 24));
+        hour = Math.floor(intDiff / (60 * 60)) - (day * 24);
+        minute = Math.floor(intDiff / 60) - (day * 24 * 60) - (hour * 60);
+        second = Math.floor(intDiff) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60);
+    }
+    if(hour<=9)   hour='0'+hour;
+    if (minute <= 9) minute = '0' + minute;
+    if (second <= 9) second = '0' + second;
+    $('#huors').html(hour);
+    $('#minter').html(minute);
+    $('#second').html(second);
+  
+    intDiff--;
+    }, 1000);
+} 
+$(function(){
+    timer(intDiff);
+}); 
+})
+
